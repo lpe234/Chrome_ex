6014a7523b09b02a62ca22d0ab3b2344083ac064
lpe234 <lpe234@qq.com>
2014-04-27 21:39:54 +0800

    1.0

---
+++ javascript/until.js
@@ -0,0 +1,51 @@
+/**
+* dizzy
+* 2014年4月26日21:46:32
+*/
+
+/* about me */
+var about_title = "<a href=\"http:\/\/dizzyblog.duapp.com/\" target=\"_blank\">by Dizzy!<\/a>";
+
+/* get obj */
+var column1 = $('.column1');
+var column2 = $('.column2');
+var column3 = $('.column3');
+var column4 = $('.column4');
+var pause_ad = $('.pause-ad');
+
+
+
+/* add mark */
+var about = $('.down-mobile');
+
+/* if has ads */
+function hasAds(obj){
+	return (obj.length>0);
+}
+
+/* hide ads */
+function hideAds(obj){
+	obj.css("display","none");
+}
+
+/* add mark */
+function addMark(obj){
+	obj.html(about_title);
+}
+
+/*-------------- to hide ads----------------*/
+if(hasAds(column4)){
+	hideAds(column4);
+	addMark(about);
+}else{
+	alert("somethings wrong !");
+}
+
+/*--------------- reset -----------------*/
+
+column2.css("right",360);
+column3.css("right",0);
+column3.css("width",360);
+
+$('#lrcResize').css("z-index","-10");
+column3.css("z-index",0);
